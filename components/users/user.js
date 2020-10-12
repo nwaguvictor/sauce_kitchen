d564a34332ca09e4d32f4b890b1532bb79fe546d
Nwagu Victor <leofizzy3@gmail.com>
2020-10-12 19:47:53 +0100

    added user model

--- components/users/user.js
+++ components/users/user.js
@@ -6,6 +6,31 @@ const userSchema = new mongoose.Schema({
         require: [true, 'name is required'],
         lowercase: true,
         minlength: [5, 'name must be atleast 5 characters'],
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'email is required'],
+    },
+    password: {
+        type: String,
+        minlength: [5, 'password must be atleast 5 characters'],
+        required: [true, 'password field is required']
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'password confirm field is required'],
+        validate: {
+            validator: function (val) {
+                return this.password === val
+            },
+            message: 'passwords must match'
+        }
+    },
+    role: {
+        type: String,
+        enum: ['admin, chef, user'],
+        default: 'user'
     }
 });
 
