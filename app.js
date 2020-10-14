ab7ce0b61794051d76945fd5bb8a9f54acd2d2c7
Nwagu Victor <leofizzy3@gmail.com>
2020-10-14 18:18:14 +0100

    worked on frontend -welcome page

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.status(200).render('base', {
+    res.status(200).render('welcome', {
         title: 'Home'
     });
 });
