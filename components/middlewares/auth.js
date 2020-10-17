c0e0619c9999b7e595f483ced80696791a6dcd14
Nwagu Victor <leofizzy3@gmail.com>
2020-10-17 17:11:51 +0100

    modified: auth.protect - changed to bearer token

--- components/middlewares/auth.js
+++ components/middlewares/auth.js
@@ -7,8 +7,8 @@ const AppError = require('../../utils/appError');
 const auth = {
     protected: asyncWrapper(async (req, res, next) => {
         let token;
-        if (req.headers.authorization) {
-            token = req.headers.authorization;
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
         } else if (req.cookies.jwt) {
             token = req.cookies.jwt;
         }
