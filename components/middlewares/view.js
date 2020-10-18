40fde6477823cbb7e778875268303e6bed06155e
Nwagu Victor <leofizzy3@gmail.com>
2020-10-18 13:29:51 +0100

    frontend completion v1

--- components/middlewares/view.js
+++ components/middlewares/view.js
@@ -7,7 +7,7 @@ const { asyncWrapper } = require("../../utils/helpers");
 exports.isLoggedIn = asyncWrapper(async (req, res, next) => {
     if (req.cookies.jwt) {
         const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_KEY);
-        const auth = await User.findById(decoded.id);
+        const auth = await User.findById(decoded.id).select('+role');
 
         if (!auth) return next();
 
